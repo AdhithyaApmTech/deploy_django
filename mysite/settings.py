abba19823e690ae11036bd74b9f9767685108e12
AdhithyaApmTech <adhithya@apmtechnologies.in>
2025-02-21 11:58:13 +0530

    cors issue

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django_ckeditor_5',
     'daphne',
     'chat',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -243,3 +245,7 @@ CKEDITOR_5_CONFIGS = {
     }
 }
 CKEDITOR_5_FILE_UPLOAD_PERMISSION = "any"  # Possible values: "staff", "authenticated", "any"
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+
